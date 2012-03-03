2a39bbc1cb3174537d1b0803b61c9c8201e14f53
Suzie <suziegbartram@gmail.com>
2012-03-02 20:52:07 -0800

    added #defines for sensors

--- defines.h
+++ defines.h
@@ -2,3 +2,14 @@
 // basically constants such as pinouts, and various params
 // for tweaking
 
+#define TAPE_SENSOR_FRONT_LEFT 0
+#define TAPE_SENSOR_FRONT_CENTER 1
+#define TAPE_SENSOR_FRONT_RIGHT 2
+#define TAPE_SENSOR_REAR_LEFT 3
+#define TAPE_SENSOR_REAR_CENTER 4
+#define TAPE_SENSOR_REAR_RIGHT 5
+#define TAPE_SENSOR_SIDE 6
+
+#define HOME_BEACON_SENSOR 4
+#define FRONT_SEESAW_SENSOR 5
+#define SIDE_SEESAW_SENSOR 6
